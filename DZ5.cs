20f92065d962c7e763d28e7d9fab852cc488278c
Alena <magii.alena@yandex.ru>
2023-07-12 06:12:13 +0300

    end commit

--- DZ5.cs
+++ DZ5.cs
@@ -1,32 +1,32 @@
 ﻿// 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
-// int size = 10;
-// int [] numbers = new int [size];
-// int count = 0;
-// for (int i = 0; i < size; i++)
-// {
-//     numbers [i] = new Random().Next(100,1000);
-//     if (numbers[i] % 2 == 0)
-//     {
-//         count++;
-//     }
-// }
-// Console.WriteLine($"Массив: [ {string.Join("; ", numbers)} ]");
-// Console.WriteLine ($"В массиве количество чётных чисел: {count}");
+int size = 10;
+int [] numbers = new int [size];
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    numbers [i] = new Random().Next(100,1000);
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Массив: [ {string.Join("; ", numbers)} ]");
+Console.WriteLine ($"В массиве количество чётных чисел: {count}");
 
 // 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
-// int size = 10;
-// int [] numbers = new int [size];
-// int sum = 0;
-// for (int i = 0; i < size; i++)
-// {
-//     numbers [i] = new Random().Next(100,1000);
-//     if (i % 2 == 1)
-//     {
-//         sum = sum + numbers [i];
-//     }
-// }
-// Console.WriteLine($"Массив: [ {string.Join("; ", numbers)} ]");
-// Console.WriteLine ($"Сумма элементов на нечетных позициях: {sum}");
+int size = 10;
+int [] numbers = new int [size];
+int sum = 0;
+for (int i = 0; i < size; i++)
+{
+    numbers [i] = new Random().Next(100,1000);
+    if (i % 2 == 1)
+    {
+        sum = sum + numbers [i];
+    }
+}
+Console.WriteLine($"Массив: [ {string.Join("; ", numbers)} ]");
+Console.WriteLine ($"Сумма элементов на нечетных позициях: {sum}");
 
 // 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
 double[] GetArray(int size, int min, int max)
